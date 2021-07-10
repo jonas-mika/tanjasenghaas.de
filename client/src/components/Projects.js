6e065d94c6df453e93834e6dff8209a32ed2d997
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-07-10 15:20:21 +0200

    Implemented Projects Image Slider

---
+++ client/src/components/Projects.js
@@ -0,0 +1,26 @@
+import ImageSlider from './image-slider/ImageSlider';
+import ArrowNavigation from './generics/ArrowNavigation';
+import { GoProject } from 'react-icons/go';
+
+const Projects = ({ images }) => {
+    return (
+        <div className="Projects main-container component">
+            <ArrowNavigation id={2} direction={'up'} />
+            <div>
+                <div className="component-header">
+                    <GoProject className="component-icon" />
+                    <h1 className="component-title">Projekte</h1>
+                    <p className="component-subtitle">
+                        Sehen Sie hier durch einige ausgewählte Projekte.
+                    </p>
+                </div>
+                <div className="component-content">
+                    <ImageSlider images={images} />
+                </div>
+            </div>
+            <ArrowNavigation id={2} direction={'down'} />
+        </div>
+    );
+};
+
+export default Projects;

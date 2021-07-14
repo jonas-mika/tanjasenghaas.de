5a6428295c547002565a8aed0ebdaea5d35de918
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-07-14 16:24:43 +0200

    Implemented AutoShow on GoUp Button

--- client/src/components/generics/GoUp.js
+++ client/src/components/generics/GoUp.js
@@ -1,15 +1,32 @@
+import { useState, useEffect } from 'react';
 import { IoIosArrowUp } from 'react-icons/io';
 
 const GoUp = () => {
+    const [showButton, setShowButton] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset > 1000) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        });
+    }, []);
+
     return (
-        <div
-            className="GoUp icon"
-            onClick={() => {
-                window.scrollTo({ top: 0, behavior: 'smooth' });
-            }}
-        >
-            <IoIosArrowUp id="icon" />
-        </div>
+        <>
+            {showButton && (
+                <div
+                    className="GoUp"
+                    onClick={() => {
+                        window.scrollTo({ top: 0, behavior: 'smooth' });
+                    }}
+                >
+                    <IoIosArrowUp id="icon" />
+                </div>
+            )}
+        </>
     );
 };
 

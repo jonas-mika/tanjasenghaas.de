b9b7d253e646a0e309105e9e7e8b727b05223b25
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-07-21 16:38:36 +0200

    Fixed Scrolling in Mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 // react imports
 import { useEffect } from 'react';
-import { isMobile } from 'react-device-detect';
+// import { isMobile } from 'react-device-detect';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Favicon from 'react-favicon';
 import Main from './pages/Main';
@@ -23,12 +23,6 @@ function App() {
         document.title = title;
     }, []);
 
-    //
-    let stopOverflowOnMobile = {};
-    if (isMobile) {
-        stopOverflowOnMobile = { overflowX: 'hidden' };
-    }
-
     useEffect(() => {
         const classes = document.body.classList;
         let timer = 0;
@@ -50,7 +44,7 @@ function App() {
 
     return (
         <Router>
-            <div className="App" style={stopOverflowOnMobile}>
+            <div className="App">
                 <Favicon url={favicon} />
                 <Switch>
                     <Route

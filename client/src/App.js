c70da6ed3fdecee085d91f5a0390294dcb8b06e6
jonas-mika <accounts@jonassenghaas.de>
2022-03-07 20:02:39 +0100

    added AnimatedCursor and wrapped App into Flex Container for correct position of footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
+import AnimatedCursor from "react-animated-cursor";
+import useMobileDetect from "use-mobile-detect-hook";
 
 // pages
 import Home from "./pages/Home";
@@ -8,21 +10,49 @@ import Vita from "./pages/Vita";
 import Project from "./pages/Project";
 
 // global styling
-import Theme from './styling/theme';
-import Font from './styling/font';
+import Theme from "./styling/theme";
+import Font from "./styling/font";
+
+// global components
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
 
 const App = () => {
+  const mobile = useMobileDetect();
+
   return (
     <ChakraProvider theme={Theme}>
       <Font />
+      {mobile.isMobile && (
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={8}
+          color="193, 11, 111"
+          outerAlpha={0.2}
+          innerScale={0.7}
+          outerScale={5}
+        />
+      )}
+
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="vita" element={<Vita />} />
-          <Route path=":projectId" element={<Project />} />
-        </Routes>
+        <Flex
+          direction="column"
+          align="flex-start"
+          height="100vh"
+          width="100vw"
+        >
+          <Header />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="vita" element={<Vita />} />
+            <Route path=":projectId" element={<Project />} />
+          </Routes>
+
+          <Footer />
+        </Flex>
       </Router>
-    </ChakraProvider> 
+    </ChakraProvider>
   );
 };
 

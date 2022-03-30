effd967f49f9ad713116f595df13c76fe3af0ea5
jonas-mika <accounts@jonassenghaas.de>
2022-03-30 17:26:47 +0200

    addd initial loading animation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
-import { Box, Image, Flex, Text, Heading, Button, useColorMode } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Flex,
+  Text,
+  Heading,
+  Button,
+  useColorMode,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 import Hero from "../layouts/Hero";
 import PrintProjects from "../layouts/PrintProjects";
 import TypographyProjects from "../layouts/TypographyProjects";
 import Customers from "../layouts/Customers";
 
-const Home = () => {
+const MotionFlex = motion(Flex);
+const MotionBox = motion(Box);
+
+const Home = ({ data }) => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <>
-      <Box
-        pos='absolute'
-        h='100vh'
-        w='100vw'
+      <MotionBox
+        pos="absolute"
+        h="100vh"
+        w="100vw"
         zIndex={-1}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
       >
         <Image
           h="100%"
@@ -24,19 +39,22 @@ const Home = () => {
           objectFit="cover"
           src="/images/wolf/hero/hero3.jpeg"
         />
-      </Box>
-    <Flex
-      direction="column"
-      mx="auto"
-      px="3rem"
-      w="100%"
-      maxWidth={1200}
-    >
-      <Hero />
-      <PrintProjects />
-      <TypographyProjects />
-      <Customers />
-    </Flex>
+      </MotionBox>
+      <MotionFlex
+        direction="column"
+        mx="auto"
+        px="3rem"
+        w="100%"
+        maxWidth={1200}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <Hero />
+        <PrintProjects projects={data.projects} />
+        <TypographyProjects projects={data.projects} />
+        <Customers customers={data.customers} />
+      </MotionFlex>
     </>
   );
 };

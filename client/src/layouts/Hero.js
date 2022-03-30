97cf075b7d309f4f9678bd363d8fe9aca892b5a7
jonas-mika <accounts@jonassenghaas.de>
2022-03-30 10:44:19 +0200

    adjusted hero to be 100vp on page load

--- client/src/layouts/Hero.js
+++ client/src/layouts/Hero.js
@@ -1,25 +1,35 @@
 import React, { useState } from "react";
-import { Flex, Box, Heading, Image } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
 
 const Hero = () => {
   return (
     <>
-      <Flex w='100%' mt='10rem' mb='1rem'>
-        <Heading fontSize={{ sm: "1.2rem", md: "1.5rem", lg: "2rem" }} fontWeight={300}>
-          Gutes Design lebt immer von einer Haltung. Es macht sich unsichtbar,
-          es lässt Dinge weg. Und doch – braucht es am Ende Details, die Bildern
-          und Texten eine Visualität und Typographie verleihen. So wird
-          Gestaltung lebendig.
+      <Flex
+        w="100%"
+        h="80vh"
+        justify="center"
+        align="center"
+      >
+        <Heading
+          fontSize={{
+            sm: "1.4rem",
+            md: "1.6rem",
+            lg: "2rem",
+          }}
+          fontWeight={400}
+        >
+          Gutes Design lebt immer von einer Haltung. Es
+          macht sich unsichtbar, es lässt Dinge weg. Und
+          doch – braucht es am Ende Details, die Bildern und
+          Texten eine Visualität und Typographie verleihen.
+          So wird Gestaltung lebendig.
         </Heading>
       </Flex>
-      <Box h={{"sm": 500, "md": 600, "lg": 800}} ml='calc(50% - 50vw)' mr='calc(50% - 50vw)'>
-        <Image
-          h="100%"
-          w="100%"
-          objectFit="cover"
-          src="/images/wolf/hero/hero2.jpeg"
-        />
-      </Box>
     </>
   );
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,43 @@
 import React, { useState } from "react";
-import { Flex, Text, Heading, Button, useColorMode } from "@chakra-ui/react";
+import { Box, Image, Flex, Text, Heading, Button, useColorMode } from "@chakra-ui/react";
 
 import Hero from "../layouts/Hero";
-import ProjectGallery from "../layouts/ProjectGallery";
+import PrintProjects from "../layouts/PrintProjects";
+import TypographyProjects from "../layouts/TypographyProjects";
+import Customers from "../layouts/Customers";
 
 const Home = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
+    <>
+      <Box
+        pos='absolute'
+        h='100vh'
+        w='100vw'
+        zIndex={-1}
+      >
+        <Image
+          h="100%"
+          w="100%"
+          opacity={0.6}
+          objectFit="cover"
+          src="/images/wolf/hero/hero3.jpeg"
+        />
+      </Box>
     <Flex
       direction="column"
       mx="auto"
-      px="2rem"
+      px="3rem"
       w="100%"
       maxWidth={1200}
     >
       <Hero />
-      <ProjectGallery />
+      <PrintProjects />
+      <TypographyProjects />
+      <Customers />
     </Flex>
+    </>
   );
 };
 

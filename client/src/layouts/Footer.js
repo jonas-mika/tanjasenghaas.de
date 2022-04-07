e7dd7bfd8fab90500a8d8d856f51a1293da71d7e
jonas-mika <accounts@jonassenghaas.de>
2022-04-07 09:39:32 +0200

    changed font style in footer + added link to www.jonas-mika.de

--- client/src/layouts/Footer.js
+++ client/src/layouts/Footer.js
@@ -30,6 +30,23 @@ const SocialLink = ({ text, url, icon = BiError }) => {
   );
 };
 
+const FooterText = ({ text, ...props }) => {
+  return (
+    <Text
+      fontSize={[
+        ".7rem",
+        ".8rem",
+        ".9rem",
+        "1em",
+        "1.1rem",
+      ]}
+      {...props}
+    >
+      {text}
+    </Text>
+  );
+};
+
 const Footer = () => {
   const gray = useColorModeValue(
     "blackAlpha.700",
@@ -42,82 +59,48 @@ const Footer = () => {
       w="100%"
       py="3rem"
       mx="auto"
-      px={{ sm: "2rem", md: "3rem" }}
+      px={["1.5rem", "3rem"]}
       maxWidth={1200}
       align="flex-start"
       justify="space-between"
     >
       <Flex direction="column">
-        <Text
-          fontSize={[
-            ".6rem",
-            ".7rem",
-            ".8rem",
-            ".9em",
-            "1rem",
-          ]}
+        <FooterText
+          text="Tanja Senghaas"
           fontWeight={700}
           mb=".2rem"
-        >
-          Tanja Senghaas
-        </Text>
-        <Text
-          fontSize={[
-            ".6rem",
-            ".7rem",
-            ".8rem",
-            ".9rem",
-            "1rem",
-          ]}
+        />
+        <FooterText
+          text="Horandstieg 30"
           fontWeight={300}
-          lineHeight="17px"
-        >
-          {" "}
-          Horandstieg 30
-        </Text>
-        <Text
-          fontSize={[
-            ".6rem",
-            ".7rem",
-            ".8rem",
-            ".9rem",
-            "1rem",
-          ]}
+        />
+        <FooterText
+          text="22559 Hamburg, Deutschland"
           fontWeight={300}
-          lineHeight="17px"
-        >
-          22559 Hamburg, Deutschland
-        </Text>
+        />
         <Link href="mailto:tanja.senghaas@web.de">
-          <Text
-            fontSize={[
-              ".6rem",
-              ".7rem",
-              ".8rem",
-              ".9rem",
-              "1rem",
-            ]}
+          <FooterText
+            text="tanja.senghaas@web.de"
             fontWeight={300}
-            lineHeight="17px"
-          >
-            tanja.senghaas@web.de
-          </Text>
+          />
         </Link>
         <Flex mt=".5rem">
-          <Text
-            color={gray}
-            fontSize={[
-              ".5rem",
-              ".6rem",
-              ".7rem",
-              ".8rem",
-              ".9rem",
-            ]}
-            fontWeight={300}
-            fontStyle="italic"
-          >
-            &copy; 2022 Jonas-Mika Senghaas
-          </Text>
+          <Link href="https://www.jonas-mika.de/">
+            <Text
+              color={gray}
+              fontSize={[
+                ".5rem",
+                ".6rem",
+                ".7rem",
+                ".8rem",
+                ".9rem",
+              ]}
+              fontWeight={300}
+              fontStyle="italic"
+            >
+              &copy; 2022 Jonas-Mika Senghaas
+            </Text>
+          </Link>
         </Flex>
       </Flex>
       <Flex direction="column">
